6805313dcdc2039604232f3dd9f80249e93a6ab9
Joaquim Perez <jquim1@hotmail.com>
2021-04-02 12:24:16 +0100

    tf 1.5 adjustments

--- model/service.py
+++ model/service.py
@@ -27,7 +27,7 @@ class ModelService:
         min_travel_distance = ModelService.TRAVEL_DISTANCE_AVG - ModelService.TRAVEL_DISTANCE_STDDEV
         max_travel_distance = ModelService.TRAVEL_DISTANCE_AVG + ModelService.TRAVEL_DISTANCE_STDDEV
 
-        tf_random = tf.random.uniform(
+        tf_random = tf.random_uniform(
                 shape=shape,
                 minval=min_travel_distance,
                 maxval=max_travel_distance,
@@ -37,8 +37,8 @@ class ModelService:
         )
         tf_var = tf.Variable( tf_random )
 
-        tf_init = tf.compat.v1.global_variables_initializer( )
-        tf_session = tf.compat.v1.Session( )
+        tf_init = tf.global_variables_initializer( )
+        tf_session = tf.Session( )
         tf_session.run( tf_init )
 
         tf_return = tf_session.run( tf_var )
